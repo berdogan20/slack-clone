47f14ef418a2c4d19b5c01c4f13b1afd9c877b8e
berdogan20 <berdogan20@ku.edu.tr>
2024-08-30 12:15:16 +0300

    toolbar component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,15 +1,17 @@
+"use client";
+
 import React from 'react'
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import { useGetWorkspace } from '@/features/workspaces/api/use-get-workspace';
 
-interface WorkspaceIdPageProps {
-    params: {
-        workspaceId: string;
-    };  
-}
+const WorkspaceIdPage = () => {
 
-const WorkspaceIdPage = ({params} : WorkspaceIdPageProps) => {
+  const workspaceId = useWorkspaceId();
+  const { data } = useGetWorkspace({ id: workspaceId });
+
   return (
     <div>
-      ID: {params.workspaceId}
+      Data: {JSON.stringify(data)}
     </div>
   )
 }
