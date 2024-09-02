84b966bb97fbef9bf66e65836070398107a24d91
berdogan20 <berdogan20@ku.edu.tr>
2024-09-02 11:00:11 +0300

    channel header component

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -33,13 +33,13 @@ const WorkspaceIdPage = () => {
 
     }, [member, memberLoading, isAdmin, channelId, workspaceLoading, channelsLoading, workspace, open, setOpen, router, workspaceId]);
 
-    if (workspaceLoading || channelsLoading) {
+    if (workspaceLoading || channelsLoading || memberLoading) {
         return (<div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
             <Loader className="size-6 animate-spin text-muted-foreground"/>
         </div>)
     }
     
-    if (!workspace) {
+    if (!workspace || !member) {
         return (<div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
             <TriangleAlert className="size-6 animate-spin text-muted-foreground"/>
             <span className="text-sm text-muted-foreground">
